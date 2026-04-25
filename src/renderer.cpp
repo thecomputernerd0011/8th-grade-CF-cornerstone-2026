1803e33ae45985c8584349502ab21b946c8c7f6f
Maddox <maddoxcooperjones@gmail.com>
2026-04-24 20:03:29 -0600

    Parser branch is a little shit, so I have to write my changes here

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -1,7 +1,17 @@
 #include "renderer.hpp"
+#include <cstdio>
+
+void Menu::promptUser(){
+    int childID = 1;
+    for(MenuOption* child : Children){
+        printf("\n\t%i. %s\n", childID, child->text.c_str());
+        childID++;
+    }
+}
 
 void Menu::open(){
     printf("%s\n%s", Title.c_str(), Body.c_str());
+    promptUser();
 }
 
 void startRenderer(Menu* mainMenu){

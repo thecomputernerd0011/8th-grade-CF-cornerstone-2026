3bba9404ce59d0055d880783f2d4adaed5459504
Maddox <maddoxcooperjones@gmail.com>
2026-04-28 21:00:37 -0600

    Added nessisary quality of life features, including a centered title.

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -1,17 +1,32 @@
 #include "renderer.hpp"
 #include <cstdio>
+#include <sys/ioctl.h>
+#include <unistd.h>
 
+bool goBack = false;
 void menu::promptUser(){
     unsigned long childID = 1;
     for(menuOption* child : Children){
         printf("\n\t%zu. %s\n", childID, child->text.c_str());
         childID++;
     }
-    printf("Enter a number (1-%zu): ", Children.size());
+    printf("\n\t0. Go back\n\nEnter a number (0-%zu): ", Children.size());
     unsigned long choice;
     scanf("%zu", &choice);
 
+    if (choice == 0){
+        goBack = true;
+        return;
+    }
+
     Children[choice - 1]->Destination->open();
+}
+
+void printCentered(const std::string& text){
+    struct winsize w;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+    int padding = (w.ws_col - text.size()) / 2;
+    printf("%*s%s\n", padding, "", text.c_str());
 }
 
 void pagedPrint(const std::string& text){
@@ -19,7 +34,7 @@ void pagedPrint(const std::string& text){
     int start = 0;
     for(size_t i = 0; i <= text.size(); i++){
         if(text[i] == '\n' || text[i] == '\0'){
-            printf("%.*s\n", i - start, text.c_str() + start);
+            printf("%.*s\n", (int)(i - start), text.c_str() + start);
             start = i + 1;
             lineCount++;
             if(lineCount % 20 == 0){
@@ -31,17 +46,22 @@ void pagedPrint(const std::string& text){
 }
 
 void menu::open(){
-    printf("%s\n", Title.c_str());
+    while (!goBack){
+        system("clear");
+        printf("\n");
+        printCentered(Title); // "centered text"
 
 #ifdef __unix__
-    FILE* pager = popen("less", "w");
-    fprintf(pager, "%s", Body.c_str());
-    pclose(pager);
+        FILE* pager = popen("less -F -X", "w");
+        fprintf(pager, "%s", Body.c_str());
+        pclose(pager);
 #else
-    pagedPrint(Body);
+        pagedPrint(Body);
 #endif
 
-    promptUser();
+        promptUser();
+    }
+    goBack = false;
 }
 
 void startRenderer(menu* mainMenu){

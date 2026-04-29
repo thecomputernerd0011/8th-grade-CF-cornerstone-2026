50e54741909dc90f230a1d117a7bd79bfc3c3508
Maddox <maddoxcooperjones@gmail.com>
2026-04-28 20:09:28 -0600

    Added makefile, made sure stuff works, compiled and ran program for the first time.

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -1,22 +1,30 @@
 #include "renderer.hpp"
 #include <cstdio>
 
+bool goingBack = false;
+
 void menu::promptUser(){
     unsigned long childID = 1;
     for(menuOption* child : Children){
         printf("\n\t%zu. %s\n", childID, child->text.c_str());
         childID++;
     }
-    printf("Enter a number (1-%zu): ", Children.capacity());
+    printf("Enter a number (0 to go back)(0-%zu): ", Children.capacity());
     unsigned long choice;
     scanf("%zu", &choice);
-
+    if(choice == 0){
+        goingBack = true;
+        return;
+    }
     Children[choice - 1]->Destination->open();
 }
 
 void menu::open(){
-    printf("%s\n%s", Title.c_str(), Body.c_str());
-    promptUser();
+    while (!goingBack){
+        printf("%s\n%s", Title.c_str(), Body.c_str());
+        promptUser();
+    }
+    goingBack = false;
 }
 
 void startRenderer(menu* mainMenu){

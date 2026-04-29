5f8edbf18ed006b5a9f272d38f4ed10db6b24b60
Maddox <maddoxcooperjones@gmail.com>
2026-04-28 20:36:27 -0600

    I'm getting pissed at this "GDamn Idiotic Truckload of shit". I'm using this branch to make this change

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -1,30 +1,47 @@
 #include "renderer.hpp"
 #include <cstdio>
 
-bool goingBack = false;
-
 void menu::promptUser(){
     unsigned long childID = 1;
     for(menuOption* child : Children){
         printf("\n\t%zu. %s\n", childID, child->text.c_str());
         childID++;
     }
-    printf("Enter a number (0 to go back)(0-%zu): ", Children.capacity());
+    printf("Enter a number (1-%zu): ", Children.size());
     unsigned long choice;
     scanf("%zu", &choice);
-    if(choice == 0){
-        goingBack = true;
-        return;
-    }
+
     Children[choice - 1]->Destination->open();
 }
 
-void menu::open(){
-    while (!goingBack){
-        printf("%s\n%s", Title.c_str(), Body.c_str());
-        promptUser();
+void pagedPrint(const std::string& text){
+    int lineCount = 0;
+    int start = 0;
+    for(size_t i = 0; i <= text.size(); i++){
+        if(text[i] == '\n' || text[i] == '\0'){
+            printf("%.*s\n", i - start, text.c_str() + start);
+            start = i + 1;
+            lineCount++;
+            if(lineCount % 20 == 0){
+                printf("-- press enter for more --");
+                getchar();
+            }
+        }
     }
-    goingBack = false;
+}
+
+void menu::open(){
+    printf("%s\n", Title.c_str());
+
+#ifdef __unix__
+    FILE* pager = popen("less", "w");
+    fprintf(pager, "%s", Body.c_str());
+    pclose(pager);
+#else
+    pagedPrint(Body);
+#endif
+
+    promptUser();
 }
 
 void startRenderer(menu* mainMenu){
